bf0d661846f7a41b49c6cbc799bc4ce94766f79e
ziruihao <ziray.hao.22@dartmouth.edu>
2020-09-05 14:41:42 -0400

    Reconfiguring constants

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv';
 import packageInfo from '../package.json';
 
+dotenv.config({ silent: true });
+
 export const PORT = process.env.PORT || 9090;
-export const MONGODB_URI = process.env.MONGODB_URI || packageInfo.localDatabaseURI;
-export const SELF_URL = process.env.NODE_ENV === 'development' ? `localhost:${9090}` : packageInfo.productionURL;
-export const APP_URL = process.env.NODE_ENV === 'development' ? `localhost:${8080}` : packageInfo.productionClientURL;
+export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/dplanner-data';
+export const SELF_URL = process.env.NODE_ENV === 'development' ? `http://localhost:${9090}` : packageInfo.productionURL;
