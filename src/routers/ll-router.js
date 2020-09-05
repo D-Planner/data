08e585ad56f741e2deee9fe214e97f1517d9bb0d
ziruihao <ziray.hao.22@dartmouth.edu>
2020-09-05 14:42:37 -0400

    Built route for SQL queries

--- src/routers/ll-router.js
+++ src/routers/ll-router.js
@@ -1,41 +1,14 @@
 import express from 'express';
 
-import { SCOPES, requireAuth } from '../authentication';
+import { SCOPES, requireToken, requireKey } from '../authentication';
 import { llController } from '../controllers';
 import { Resources } from '../models';
 
 const router = express();
 
-router.route('/reviews')
+router.route('/query')
 
-  .get(requireAuth([SCOPES.LL_REVIEWS.READ]), llController.readReviews)
-
-  .post(requireAuth, (req, res) => {
-    const resource = new Resources();
-
-    resource.title = req.body.title;
-    resource.description = req.body.description;
-    resource.value = req.body.value;
-    resource.date_account_created = Date.now();
-
-    resource.save()
-      .then((savedResource) => {
-        return res.json(savedResource);
-      }).catch((error) => {
-        return res.status(500).json(error);
-      });
-  })
-
-  // Delete all resources (SECURE, TESTING ONLY)
-  .delete(requireAuth, (req, res) => {
-    Resources.deleteMany({ })
-      .then(() => {
-        return res.json({ message: 'Successfully deleted all resources.' });
-      })
-      .catch((error) => {
-        return res.status(500).json(error);
-      });
-  });
+  .post(requireKey([SCOPES.LL_ALL.READ]), llController.serveQuery);
 
 router.route('/:id')
 
@@ -55,7 +28,7 @@ router.route('/:id')
   })
 
   // Update resource by id (SECURE)
-  .put(requireAuth, (req, res) => {
+  .put(requireToken([]), (req, res) => {
     Resources.updateOne({ _id: req.params.id }, req.body)
       .then(() => {
         // Fetch resource object and send
@@ -77,7 +50,7 @@ router.route('/:id')
   })
 
   // Delete resource by id, SECURE
-  .delete(requireAuth, (req, res) => {
+  .delete(requireToken([]), (req, res) => {
     Resources.deleteOne({ _id: req.params.id })
       .then(() => {
         return res.json({ message: `Resource with id: ${req.params.id} was successfully deleted` });
